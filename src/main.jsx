76a169456103428ed7901d6e28a7f55949131b0e
melikeblu <meliketufanyazici@gmail.com>
2026-06-11 17:26:38 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Marea from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Marea />
+  </React.StrictMode>,
+)
